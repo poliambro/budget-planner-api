17e789574b562c4d2fd940e67d80a6cafbabc6ad
PolianaNeves <poliana.ambrosio.campos@gmail.com>
2022-08-07 11:47:11 -0300

    spring init

---
+++ src/main/java/com/poliana/alura/challenges/budgetplannerapi/BudgetPlannerApiApplication.java
@@ -0,0 +1,13 @@
+package com.poliana.alura.challenges.budgetplannerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetPlannerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetPlannerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/poliana/alura/challenges/budgetplannerapi/BudgetPlannerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.poliana.alura.challenges.budgetplannerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetPlannerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
